f28f21709f5e1264bfd62b49bcecc7bd6504a283
PlebeianTm <ge00002@mix.wvu.edu>
2024-04-09 16:26:37 -0400

    Updating

--- SmallBusinessSystem/Controllers/CandyController.cs
+++ SmallBusinessSystem/Controllers/CandyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
 using Microsoft.EntityFrameworkCore;
 using SmallBusinessSystem.Data;
 using SmallBusinessSystem.Models;
@@ -48,5 +49,28 @@ namespace SmallBusinessSystem.Controllers
             var listOfCandy = _dbContext.Candies.ToList();
             return View(listOfCandy);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Candy candy= _dbContext.Candies.Find(id);
+
+            return View(candy);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("CandyId, CandyName, Description, CandyPrice,ImgUrl,CandyQty")] Candy candyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Candies.Update(candyObj);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index", "Candy");
+            }
+
+            return View(candyObj);
+        }
+
     }
 }
