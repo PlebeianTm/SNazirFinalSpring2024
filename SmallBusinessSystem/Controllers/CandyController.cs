b777d60bf7bdb26f3c9034e074317afa59cacfc9
SophiaHarker20 <srh00020@mix.wvu.edu>
2024-04-09 16:23:27 -0400

    added create with img functionlity to controller

--- SmallBusinessSystem/Controllers/CandyController.cs
+++ SmallBusinessSystem/Controllers/CandyController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data; 
+
 using Microsoft.EntityFrameworkCore;
 using SmallBusinessSystem.Data;
 using SmallBusinessSystem.Models;
+using System.IO;
 
 namespace SmallBusinessSystem.Controllers
 {
     public class CandyController : Controller
     {
         private CandyDbContext _dbContext;
+        private IWebHostEnvironment _enviornment;
         public CandyController(CandyDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -27,14 +31,24 @@ namespace SmallBusinessSystem.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Candy candyObj)
+        public IActionResult Create(Candy candyObj, IFormFile imgFile)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) 
             {
+                string wwwrootPath = _enviornment.WebRootPath;
+
+                if(imgFile != null) 
+                {
+                    using (var fileStream = new FileStream(Path.Combine(wwwrootPath, @"Images/CandyImages/" + imgFile.FileName), FileMode.Create))
+                    {
+                        imgFile.CopyTo(fileStream);
+                    }
+                    candyObj.ImgUrl = @"\Images\CandyImages\" + imgFile.FileName;
+                }
                 _dbContext.Candies.Add(candyObj);
                 _dbContext.SaveChanges();
 
-                return RedirectToAction("Index", "Candy");
+                return RedirectToAction("Index", "Candy"); 
             }
 
             return View(candyObj);
