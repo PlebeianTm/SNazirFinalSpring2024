9ff0d50d7d8c82d148c42f9f936277c643717d00
PlebeianTm <ge00002@mix.wvu.edu>
2024-04-09 14:36:28 -0400

    Creating Candy Controller

--- SmallBusinessSystem/Controllers/CandyController.cs
+++ SmallBusinessSystem/Controllers/CandyController.cs
@@ -1,6 +1,34 @@
-﻿namespace SmallBusinessSystem.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmallBusinessSystem.Controllers
 {
-    public class CandyController
+    public class CandyController : Controller
     {
+        public CandyController(Candy dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+        public IActionResult Index() //right click index to create folder 
+        {
+            var listOfCandy = _dbContext.Candy.ToList();
+
+            return View(listOfCandy);
+        }
+        [HttpPost]
+        public IActionResult Create(Candy candyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Candy.Add(candyObj);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index", "Candy");
+            }
+
+            return View(candyObj);
+
+        }
     }
 }
