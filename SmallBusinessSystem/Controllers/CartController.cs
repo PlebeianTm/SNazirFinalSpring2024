d31284eb3a164a2ae36f6cad6927d0b6f5c76ca6
PlebeianTm <ge00002@mix.wvu.edu>
2024-04-24 18:37:26 -0400

    Creating Candy Model

---
+++ SmallBusinessSystem/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmallBusinessSystem.Data;
+
+namespace SmallBusinessSystem.Controllers
+{
+    public class CartController : Controller
+    {
+        private CandyDbContext _dbContext;
+        public CartController(CandyDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+    }
+
+}
